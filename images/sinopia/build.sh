af29f3f3c31ceb3aa3df1d8eba91a363815a662f
Mike Pham <mpham@nativecode.com>
2016-05-22 22:20:33 +0000

    sinopia

--- images/sinopia/build.sh
+++ images/sinopia/build.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-$SCRIPTDIR=$PWD
-
-cd /var/opt/infrastructure/images/sinopia
-clear;
-
+echo "Building container..."
 docker build -t test/sinopia:latest .;
+
+echo "Running container..."
 docker run --name test-sinopia --rm test/sinopia:latest;
 
 read -p "Press [any] key to clean up."
 
+docker stop test-sinopia;
 docker rm test-sinopia;
 docker rmi test-sinopia
-clear;
-
-cd $PWD
