5fed4ccf42bca07ea9cf6714399c2100df1e0f8a
Mike Pham <mpham@nativecode.com>
2016-05-24 17:08:03 +0000

    adding setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O /usr/local/bin/infrastructure-update.sh \
+  https://www.dropbox.com/s/kxx3h578i6bqzkl/infrastructure-update.sh?dl=0;
+chmod +x /usr/local/bin/infrastructure-update.sh;
