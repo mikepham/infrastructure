9ef3c6af9c0653dd48f3e3606a8c702b195d43af
Mike Pham <mpham@nativecode.com>
2016-05-25 05:12:16 +0000

    added bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+BASEPATH=/opt/infrastructure
+
+##
+# If the repository hasn't been installed yet, we have
+# to download it via git, which we'll check exists and
+# install from apt if not.
+if [ ! -d "/opt/infrastructure" ]; then
+  GIT=`which git`;
+  if [ $? -gt 0 ]; then
+    apt-get update;
+    apt-get install -y git;
+  fi
+  git clone --recursive https://github.com/nativecode-dev/infrastructure.git $BASEPATH;
+  chmod +x $BASEPATH/infrastructure.sh;
+  chmod +x $BASEPATH/setup.sh;
+  chmod +x $BASEPATH/sync.sh;
+fi
+
+$BASEPATH/infrastructure.sh setup;
